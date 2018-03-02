1c57d8d47bac0977f43a60f487021605d00b5c6f
Jimmy Joseph <jimmyjoseph@outlook.com>
2018-03-02 19:37:10 +0000

    initial commit

---
+++ src/cs551k/examples/rmi/mailbox/negotation/singlebuyer/MailboxInterface.java
@@ -0,0 +1,18 @@
+//package hello;
+package cs551k.examples.rmi.mailbox.negotation.singlebuyer;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * @author Martin Kollingbaum
+ * University of Aberdeen
+ * 2018
+ *
+ */
+public interface MailboxInterface extends Remote
+{
+  public Message receive(String agentname) throws RemoteException ;
+  public void send(Message message) throws RemoteException;
+
+}
