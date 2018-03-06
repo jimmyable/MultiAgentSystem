27c93a4822bb9b13a0d9a4e6b37677b976b4ecef
Jimmy Joseph <jimmyjoseph@outlook.com>
2018-03-06 11:30:42 +0000

    added A LOT MORE comments

--- CS551K/src/cs551k/examples/rmi/mailbox/negotiation/singlebuyer/Mailbox.java
+++ CS551K/src/cs551k/examples/rmi/mailbox/negotiation/singlebuyer/Mailbox.java
@@ -17,13 +17,11 @@ import java.util.Iterator;
  */
 public class Mailbox implements MailboxInterface	//hello darkness my old friend
 {
-  private ArrayList<Message> messageList = new ArrayList<Message>(); //this array only accepts messages
+  private ArrayList<Message> messageList = new ArrayList<Message>(); //create a new arraylist that stores messages
 
-  /**
-   * A polar bear's skin is black. Its fur is not white, but actually clear
-   */
-  public Mailbox() {		//we got a public class over here
-    // TODO Auto-generated constructor stub
+  
+  public Mailbox() {		//we got a public class over here, MAILBOX
+    // TODO Auto-generated constructor stub QUE?
   }
 
 
@@ -61,42 +59,42 @@ public class Mailbox implements MailboxInterface	//hello darkness my old friend
   /**
    * @param args
    */
-  public static void main(String[] args) 
+  public static void main(String[] args) //here be the main method, takes in two args
   {
     // look at the server example and fill in all the required statements to register
     // a mailbox remote object with the rmiregistry
 	  
-	  if (args.length < 2) {
+	  if (args.length < 2) {	//if not given 2 args throw error
           System.err.println( "Server: wrong number of paramters,required: <registryport> <serverport>" ) ;
           return;
      }
-     try
+     try //else
      {
-        String hostname = (InetAddress.getLocalHost()).getCanonicalHostName() ;
-        int registryport = Integer.parseInt( args[0] ) ;
-        int serviceport = Integer.parseInt( args[1] );
+        String hostname = (InetAddress.getLocalHost()).getCanonicalHostName() ; //get hostname using magic
+        int registryport = Integer.parseInt( args[0] ) ;						//set first arg as the registry port
+        int serviceport = Integer.parseInt( args[1] );							//set second arg as the service port
 
-        System.out.println ( "Server> hostname = " + hostname ) ;
-        System.out.println ( "Server> registry port = " + registryport ) ;
-        System.out.println ( "Server> service port = " + serviceport ) ;
+        System.out.println ( "Server> hostname = " + hostname ) ;				//print out the magic hostname
+        System.out.println ( "Server> registry port = " + registryport ) ;		//print out the ports that we just initiated
+        System.out.println ( "Server> service port = " + serviceport ) ;		//same with the other port
 
-        System.setProperty( "java.security.policy", "security.policy" ) ;
+        System.setProperty( "java.security.policy", "security.policy" ) ;		//java security stuff to mess about with rmi etc.
         System.setSecurityManager( new SecurityManager() ) ;
 
         // ===============================================
         // this instantiates the remote object
-        Mailbox mailbox = new Mailbox() ;
+        Mailbox mailbox = new Mailbox() ;										//now we are going to make the actual object
         //
         // ===============================================
 
         // export remote object at a particular port
         // if serviceport == 0, then Java will find the next available port
-        MailboxInterface stub = (MailboxInterface) UnicastRemoteObject.exportObject(mailbox, serviceport) ;
+        MailboxInterface stub = (MailboxInterface) UnicastRemoteObject.exportObject(mailbox, serviceport); //link up the mailbox to the interface using stub
         // register the remote object with the rmiregistry with the object's stub
-        String regURL = "rmi://" + hostname + ":" + registryport + "/MailboxService";
-        Naming.rebind(regURL, stub) ;
+        String regURL = "rmi://" + hostname + ":" + registryport + "/MailboxService"; //print out stuff to look cool
+        Naming.rebind(regURL, stub) ;												//do stuff to find remote location or summin
 
-        System.out.println ( "Server ready." ) ;
+        System.out.println ( "Server ready." ) ;									//WE READY
      }
      catch (java.net.UnknownHostException e)
      {
--- CS551K/src/cs551k/examples/rmi/mailbox/negotiation/singlebuyer/Message.java
+++ CS551K/src/cs551k/examples/rmi/mailbox/negotiation/singlebuyer/Message.java
@@ -1,5 +1,5 @@
-//package hello;
-package cs551k.examples.rmi.mailbox.negotiation.singlebuyer;
+//package hello
+package cs551k.examples.rmi.mailbox.negotiation.singlebuyer; //declare the package
 
 import java.io.Serializable;
 
@@ -10,36 +10,38 @@ import java.io.Serializable;
  *
  */
 
-public class Message implements Serializable
+public class Message implements Serializable		//do public class
 {
-  private String receiver = null ;
-  private String message  = null ;
+  private String receiver = null ;					//initilaise reciever
+  private String message  = null ;					//initiliase message
   /**
    * 
    */
-  public Message() {} 
-  public Message(String receiver, String message )
+  public Message() {} 								//empty message constructor
+  public Message(String receiver, String message )	//message constructor that takes in message and reciever
   {
-    this.receiver  = receiver ;
+    this.receiver  = receiver ;						//if this is how the object is called then this.that
     this.message   = message ;
   }
+  
   /**
    * @return the message
    */
-  public String getMessage() {
+  public String getMessage() {						//call this method to display the actual "message"
     return message;
   }
   /**
    * @param message the message to set
    */
-  public void setMessage(String message) {
+  public void setMessage(String message) {		//call this method to set a "message", requires a message to be fed in
     this.message = message;
   }
-  public String getReceiver() {
+  public String getReceiver() {					//call this method to get the "reciever" name
     return receiver;
   }
-  public void setReceiver(String receiver) {
+  public void setReceiver(String receiver) {	//call this method to set the "reciever" name
     this.receiver = receiver;
   }
+  
 
 }
